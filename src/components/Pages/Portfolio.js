e5ab7d51aa08a900d39a6862161c4e80062aa23d
Brennan Predmore <brennnann@gmail.com>
2020-04-26 00:56:37 -0500

    footer styling init // github repo link init in footer // portfolio row 1 without style

--- src/components/Pages/Portfolio.js
+++ src/components/Pages/Portfolio.js
@@ -3,8 +3,30 @@ import React from 'react'
 function Portfolio() {
     return (
         <div>
-            
-        </div>
+              <h1><u>Projects</u></h1>
+              <div class="container"/>
+    <div class="row"/>
+    <div class="card col-md-3 col-12"/>
+      <h2 id="cardTitle">Trustafarian World Wide</h2>
+      <a id="gitRepo" href="https://github.com/BrennanPredmore/project_1">Github Repository</a>
+      <a href="https://brennanpredmore.github.io/project_1/"/>
+        <img class="img-fluid" src="https://media.giphy.com/media/ZA6BQajWQV6y8HYQgn/giphy.gif" alt="projectOne" id="projectOne"/>
+
+
+    <div class="card col-md-3 col-12"/>
+      <h2 id="cardTitle">Zeldas Cut-Throat Kitchen</h2>
+      <a id="gitRepo" href="https://github.com/BrennanPredmore/project2">Github Repository</a>
+      <a href="https://zelda-cut-throat-kitchen.herokuapp.com/"/>
+        <img class="img-fluid" src="https://media.giphy.com/media/U2S8YrlEWxuyeTMV0P/giphy.gif" alt="zeldaGIF"
+          id="zeldaGIF"/>
+
+ 
+    <div class="card col-md-3 col-12"/>
+      <h2 id="cardTitle">Code Quiz</h2>
+      <a id="gitRepo" href="https://github.com/BrennanPredmore/codeQuiz">Github Repository</a>
+      <a href="https://brennanpredmore.github.io/codeQuiz/"/>
+        <img class="img-fluid" src="https://media.giphy.com/media/iJgOcYFBuZZOUL5CFN/giphy.gif" alt="codeQuiz" id="codeQuiz"/>
+</div>
     )
 }
 
