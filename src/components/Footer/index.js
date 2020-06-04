2192523e2623458a34635e31715d8b240b65b123
Brennan Predmore <brennnann@gmail.com>
2020-06-04 03:49:17 -0500

    update information in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -18,13 +18,15 @@ function index() {
           <a href="https://github.com/BrennanPredmore"><FontAwesomeIcon icon={faGithubAlt} /></a>
         <a href="https://www.linkedin.com/in/brennan-predmor%C3%A9-132306195/"><FontAwesomeIcon icon={faLinkedin} /></a></div>
         <div className="col-sm-6 col-md-3 item" style={{height: 64, minHeight: 64}}>
-          <h3 style={{marginBottom: 0}}>Services</h3>
+          <h3 style={{marginBottom: 0}}>Full Stack Services Contact:</h3>
           <ul>
-            <li>React.js/MERN</li>
+            <li>Email: brennnann@gmail.com</li>
+            <li>Phone: 5857045630</li>
+
           </ul>
         </div>
         <div className="col-md-6 item text" style={{maxWidth: 266, height: 64, minHeight: 64}}>
-          <h3 style={{marginBottom: 0}}>Brennan Predmoré Portfolio© 2020</h3>
+          <h3 style={{marginBottom: 0}}>Brennan Predmoré Portfolio © 2020</h3>
         </div>
       </div>
     </div>
