dc8e13be534a57134f41efc07886bf8482368a0c
Brennan Predmore <brennnann@gmail.com>
2020-06-22 02:59:50 -0500

    update aboutme (brand statement) paragraph

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -11,29 +11,28 @@ function index() {
 
 
  <p>
-   Brennan is a former music teacher that is a graduate of The University of Texas at Austin - Full Stack Software Development Bootcamp.
-   He is originally from Rochester, New York and moved to Austin six years ago after he graduated from a music education
-   program.
-   He hopes to become a full stack software developer/engineer one day and change the world around him!
+ I am a graduate of the University of Texas at Austin - Full Stack Software Development Bootcamp. Originally from Rochester, New York
+  I moved to Austin six years ago after I graduated from a music education program. With proven success in the field, I’ve become a 
+  full stack software engineer and continue to change the world around me!
    <br/>
-   He found love in coding and learning new languages shortly after graduating school.
-   Brennan had been bilingual from birth knowing Italian and english as his primary languages.
-   As time went on in his programming studies, Brennan began to notice the similarities between creating musical
-   notation and writing code.
+   I found love in coding and learning new languages shortly after graduating music school. I had also been bilingual from birth knowing 
+   Italian and English as my primary languages. As time went on in my programming studies, I began to notice the similarities between creating 
+   musical notation, learning other languages and writing code.
+   <br/>
+   After many years of hard work and schooling I was able to learn many different languages and frameworks along the way including but not 
+   limited to: JavaScript, React.js, React-Native, Redux, HTML5, CSS, SASS, Bootstrap, JQuery, MySQL, Node.js, Heroku, Git, CRUD operations, 
+   RESTful API’s, MongoDB, and PWA. I still continue to strive to learn more and sharpen my skills.
    <br />
-   They both demanded a lot of discipline, strict rules, and creativity to construct your final content.
-   Ever since Brennan was child he wanted to create and change the world around him.
-   Brennan had a very strong urge to share the knowledge he had learned through his own music education as well as
-   software development with others around him.
-   Once he graduated college Brennan began teaching privately for 5 years at a school in north Austin.
-   He was instructing all ages in percussion, music theory/composition and piano during that time.
-   He has also played in numerous successful bands all over the country and is still currently gigging around Texas.
-   Brennan has a strong emphasis on technique and learning the fundamentals of any of his chosen fields of study.
+   They all demanded a lot of discipline, strict rules, and creativity to construct your final content. Ever since I was child I wanted to 
+   create and use my curiosity to learn and grow. I had a very strong urge to share the knowledge I’d learned through my own music education 
+   as well as software development with others. Once I graduated college I began teaching privately for 5 years at a school in north Austin. 
+   I was instructing all ages in percussion, music theory/composition and piano during that time. 
    <br />
-   "I work and practice very hard alongside my peers to achieve any goal we set out to accomplish.
-   I personally believe that if I am learning, having fun, and my team is with me every step of the way, we will in
-   turn be successful!"
+   I have also played in numerous successful bands all over the country and is still currently gigging around Texas. I have a strong emphasis 
+   on technique and learning the fundamentals of any of his chosen fields of study.
  <br />
+ "I work and practice very hard alongside my peers to achieve any goal we set out to accomplish. I personally believe that if I am learning, 
+ having fun, and my team is with me every step of the way, we will in turn be successful!"
  </p>
 
         </div>
