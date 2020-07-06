76ee62dea7c21f5d1e5b7f6a1c0c251e1b0abc3c
Brennan Predmore <brennnann@gmail.com>
2020-07-06 16:43:21 -0500

    lato font-family init for about me section

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -11,7 +11,14 @@ function index() {
 
 
  <p>
- Full Stack developer and a graduate of the University of Texas at Austin’s Full Stack Software Development Bootcamp. I found a love of coding and learning new languages shortly after graduating from Finger Lakes Community College with an Associates degree in music. Life long learner with expertise in JavaScript, React.js, React-Native, Redux, HTML5, CSS, SASS, Bootstrap, JQuery, MySQL, Node.js, Heroku, Git, CRUD operations, RESTful API’s, MongoDB, and PWA. Recognized as disciplined and for creating user-centric content. Proven ability to work collaboratively alongside peers to achieve company objectives. High performer looking to leverage complex problem solving skills and the ability to work effectively with diverse teams to drive organizational growth.
+ Full Stack developer and a graduate of the University of Texas at Austin’s Full Stack Software Development 
+ Bootcamp. I found a love of coding and learning new languages shortly after graduating from Finger Lakes 
+ Community College with an Associates degree in music. Life long learner with expertise in JavaScript, 
+ React.js, React-Native, Redux, HTML5, CSS, SASS, Bootstrap, JQuery, MySQL, Node.js, Heroku, Git, CRUD 
+ operations, RESTful API’s, MongoDB, and PWA. Recognized as disciplined and for creating user-centric content. 
+ Proven ability to work collaboratively alongside peers to achieve company objectives. High performer looking 
+ to leverage complex problem solving skills and the ability to work effectively with diverse teams to drive 
+ organizational growth.
  </p>
 
         </div>
