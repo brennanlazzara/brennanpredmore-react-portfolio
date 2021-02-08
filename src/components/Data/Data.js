9e538eb1141da75e9966dc65b31e645fe8a4f1fb
BrennnanPredmore <brennnann@gmail.com>
2021-02-08 17:19:34 -0600

    updated portfolio with southwest application // move around carousel order putting new applications first

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -1,87 +1,88 @@
 const data = {
-    "properties": [
-        {
-            // Jobless
-            "_id": "593e9297ec4cca9c56bf61af",
-            "index": 0,
-            "title": "Jobless",
-            
-            "picture": "https://media.giphy.com/media/cgeIncAhR7ot5ye30F/giphy.gif",
-            "about": "REACT MONGODB EXPRESS NODE",
-            "githubLink": "https://github.com/BrennanPredmore/jobless",
-            "projectLink": "https://jobless2020.herokuapp.com/"
-        },    
+  properties: [
     {
-        // Zeldas Cut-Throat Kitchen
-        "_id": "593e9297ec4cca9c56bf61af",
-        "index": 1,
-        "title": "Zeldas Cut-Throat Kitchen",
-        
-        "picture": "https://media.giphy.com/media/U2S8YrlEWxuyeTMV0P/giphy.gif",
-        "about": "HTML CSS/BOOTSTRAP JAVASCRIPT JQUERY NODE.JS EXPRESS MYSQL2 SEQUELIZE",
-        "githubLink": "https://github.com/BrennanPredmore/project2",
-        "projectLink": "https://zelda-cut-throat-kitchen.herokuapp.com/"
-    },    
+      // Southwest Education
+      _id: '593e9297ec4cca9c56bf61af',
+      index: 0,
+      title: 'Southwest Education',
+      picture: 'https://media.giphy.com/media/MDbrhKs7weDfkeNFr8/giphy.gif',
+      about: 'REACT MAPBOX-GL MONGODB EXPRESS NODE',
+      githubLink: 'https://github.com/Southwest-Education',
+      projectLink: 'https://southwest-education-cmlho.ondigitalocean.app/',
+    },
     {
-        // TRUSTAFARIAN CARD
-        "_id": "593e9297e17df20c4a237d42",
-        "index": 2,
-        "title": "Trustafarians", 
-        "picture": "https://media.giphy.com/media/ZA6BQajWQV6y8HYQgn/giphy.gif",
-        "about": "HTML CSS - BULMA JAVASCRIPT JQUERY API (SKYSCANNER, OPEN WEATHER)",
-        "githubLink": "https://github.com/BrennanPredmore/project_1",
-        "projectLink": "https://brennanpredmore.github.io/project_1/"
+      // Jobless
+      _id: '593e9297ec4cca9c56bf61af',
+      index: 1,
+      title: 'Jobless',
 
+      picture: 'https://media.giphy.com/media/cgeIncAhR7ot5ye30F/giphy.gif',
+      about: 'REACT MONGODB EXPRESS NODE',
+      githubLink: 'https://github.com/BrennanPredmore/jobless',
+      projectLink: 'https://jobless2020.herokuapp.com/',
+    },
+    {
+      // Zeldas Cut-Throat Kitchen
+      _id: '593e929773c71925e5d7c11c',
+      index: 2,
+      title: 'Zeldas Cut-Throat Kitchen',
+      picture: 'https://media.giphy.com/media/U2S8YrlEWxuyeTMV0P/giphy.gif',
+      about:
+        'HTML CSS/BOOTSTRAP JAVASCRIPT JQUERY NODE.JS EXPRESS MYSQL2 SEQUELIZE',
+      githubLink: 'https://github.com/BrennanPredmore/project2',
+      projectLink: 'https://zelda-cut-throat-kitchen.herokuapp.com/',
+    },
+    {
+      // TRUSTAFARIAN CARD
+      _id: '593e9297e17df20c4a237d42',
+      index: 3,
+      title: 'Trustafarians',
+      picture: 'https://media.giphy.com/media/ZA6BQajWQV6y8HYQgn/giphy.gif',
+      about:
+        'HTML CSS - BULMA JAVASCRIPT JQUERY API (SKYSCANNER, OPEN WEATHER)',
+      githubLink: 'https://github.com/BrennanPredmore/project_1',
+      projectLink: 'https://brennanpredmore.github.io/project_1/',
+    },
 
+    {
+      // MVC Burger Resturaunt
+      _id: '593e92973d4d15eedb129aee',
+      index: 4,
+      title: 'MVC',
+      picture: 'https://media.giphy.com/media/huOgLzf13q97T0XtrC/giphy.gif',
+      about:
+        'HTML CSS/BOOTSTRAP JAVASCRIPT JQUERY NODE.JS EXPRESS EXPRESS-HANDLEBARS MYSQL ORM',
+      githubLink: 'https://github.com/BrennanPredmore/MVC',
+      projectLink: 'https://burgermvc.herokuapp.com/',
     },
     {
-        // Code Quiz
-        "_id": "593e929773c71925e5d7c11c",
-        "index": 3,
-        "title": "Code Quiz",
-        "picture": "https://media.giphy.com/media/iJgOcYFBuZZOUL5CFN/giphy.gif",
-        "about": "HTML CSS BOOTSTRAP JAVASCRIPT/JQUERY",
-        "githubLink": "https://github.com/BrennanPredmore/codeQuiz",
-        "projectLink": "https://brennanpredmore.github.io/codeQuiz/"
-
+      // EMPLOYEE TRACKER
+      _id: '593e929728b7f8543b7907e1',
+      index: 5,
+      title: 'EMPLOYEE TRACKER',
+      picture: 'https://media.giphy.com/media/kDx2eO0DkaYnP1uPD5/giphy.gif',
+      about: 'HTML CSS/BOOTSTRAP JAVASCRIPT JQUERY NODE.JS',
+      githubLink: 'https://github.com/BrennanPredmore/OOP',
+      projectLink: '',
     },
     {
-        // MVC Burger Resturaunt
-        "_id": "593e92973d4d15eedb129aee",
-        "index": 4,
-        "title": "MVC",
-        "picture": "https://media.giphy.com/media/huOgLzf13q97T0XtrC/giphy.gif",
-        "about": "HTML CSS/BOOTSTRAP JAVASCRIPT JQUERY NODE.JS EXPRESS EXPRESS-HANDLEBARS MYSQL ORM",
-        "githubLink": "https://github.com/BrennanPredmore/MVC",
-        "projectLink": "https://burgermvc.herokuapp.com/"
+      // LinkedIn
+      _id: '593e9297a13e246da5ba0884',
+      index: 6,
+      title: 'LinkedIn',
+      picture: 'assets/linkedInScreenShot.png',
+      projectLink:
+        'https://www.linkedin.com/in/brennan-predmor%C3%A9-132306195/',
     },
     {
-        // EMPLOYEE TRACKER
-        "_id": "593e929728b7f8543b7907e1",
-        "index": 5,
-        "title": "EMPLOYEE TRACKER",
-        "picture": "https://media.giphy.com/media/kDx2eO0DkaYnP1uPD5/giphy.gif",
-        "about": "HTML CSS/BOOTSTRAP JAVASCRIPT JQUERY NODE.JS",
-        "githubLink": "https://github.com/BrennanPredmore/OOP",
-        "projectLink": ""
+      // GITHUB
+      _id: '593e9297afa51a013fcd07d5',
+      index: 7,
+      title: 'Github',
+      picture: 'assets/githubScreenShot.png',
+      projectLink: 'https://github.com/BrennanPredmore',
     },
-    {
-        // LinkedIn
-        "_id": "593e9297a13e246da5ba0884",
-        "index": 6,
-        "title": "LinkedIn",
-        "picture": "assets/linkedInScreenShot.png",
-        "projectLink": "https://www.linkedin.com/in/brennan-predmor%C3%A9-132306195/"
-    },
-    {
-        // GITHUB
-        "_id": "593e9297afa51a013fcd07d5",
-        "index": 7,
-        "title": "Github",
-        "picture": "assets/githubScreenShot.png",
-        "projectLink": "https://github.com/BrennanPredmore"
-    },
-    ]
-}
+  ],
+};
 
 export default data;
